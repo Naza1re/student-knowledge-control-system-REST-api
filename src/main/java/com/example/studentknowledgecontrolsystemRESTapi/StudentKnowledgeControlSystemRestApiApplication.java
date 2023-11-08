51e5146c225a457212d28f372c4115c952e8dc86
Naza1re <mcarim@mail.ru>
2023-11-08 20:33:40 +0300

    Initial commit

---
+++ src/main/java/com/example/studentknowledgecontrolsystemRESTapi/StudentKnowledgeControlSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentknowledgecontrolsystemRESTapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentKnowledgeControlSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentKnowledgeControlSystemRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentknowledgecontrolsystemRESTapi/StudentKnowledgeControlSystemRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentknowledgecontrolsystemRESTapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentKnowledgeControlSystemRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
